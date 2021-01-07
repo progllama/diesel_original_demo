e01b5b402a5ee2045b0c1c5d9bb1001a0630c6ae
Yuta Tachihara <>
2021-01-08 03:05:05 +0900

    WIP

--- src/main.rs
+++ src/main.rs
@@ -40,18 +40,15 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut tsk = String::new();
     std::io::stdin().read_line(&mut tsk);
 
-    create_todo(&tsk);
+    // get_todo();
+
+    // update_todo();
+
+    // get_todo();
+
+    destroy_todo();
     
     Ok(())
-}
-
-pub fn establish_connection() -> PgConnection {
-    dotenv().ok();
-
-    let database_url = env::var("DATABASE_URL")
-        .expect("DATABASE_URL must be set");
-    PgConnection::establish(&database_url)
-        .expect(&format!("Error connecting to {}", database_url))
 }
 
 //---------------------------------------------------------
@@ -67,8 +64,10 @@ pub fn establish_connection() -> PgConnection {
 //prepare DBPool
 
 lazy_static! {
-    static ref DbPOOL: DbPool = {
+    static ref DBPOOL: DbPool = {
         loop {
+            dotenv().ok();
+
             let connspec = if let Ok(v) = std::env::var("DATABASE_URL") {
                 v
             } else {
@@ -84,29 +83,65 @@ lazy_static! {
     };
 }
 
+pub fn establish_connection() -> Result<impl Connection, Box<dyn std::error::Error>> {
+}
+
+// pub fn establish_connection() -> PgConnection {
+//     dotenv().ok();
+
+//     let database_url = env::var("DATABASE_URL")
+//         .expect("DATABASE_URL must be set");
+//     PgConnection::establish(&database_url)
+//         .expect(&format!("Error connecting to {}", database_url))
+// }
+
 fn create_todo(t: &String) -> Result<(), Box<dyn std::error::Error>> {
     use schema::todos::dsl::*;
-    let connection = establish_connection();
+    let connection = DBPOOL.clone().get().unwrap();
     let inserted_row = diesel::insert_into(todos)
         .values((&task.eq(t), complete.eq(false)))
         .execute(&connection)?;
     Ok(())
 }
 
-fn get_todo() {
-
+fn get_todo() -> Result<(), Box<dyn std::error::Error>> {
+    use schema::todos::dsl::*;
+    let connection = establish_connection();
+    let data = todos.select((id, task, complete)).load::<(i32, String, bool)>(&connection)?;
+    for item in data {
+        println!("id {} \ntask {} \n complete {}", item.0, item.1, item.2);
+    }
+    Ok(())
 }
 
-fn get_todos() {
-
+fn get_todos() -> Result<(), Box<dyn std::error::Error>> {
+    use schema::todos::dsl::*;
+    let connection = establish_connection();
+    let data = todos.select((task, complete)).load::<(String, bool)>(&connection)?;
+    for item in data {
+        println!("task {} \n complete {}", item.0, item.1);
+    }
+    Ok(())
 }
 
 fn update_todo() {
-
+    use schema::todos::dsl::*;
+    let connection = establish_connection();
+    let data = diesel::update(todos.filter(id.eq(1)))
+        .set((task.eq("Unko"), complete.eq(true)))
+        .execute(&connection);
+    if let Err(e) = data {
+        println!("{:#?}", e);
+    }
 }
 
-fn destroy_todo() {
-
+fn destroy_todo() -> Result<(), Box<dyn std::error::Error>> {
+    use schema::todos::dsl::*;
+    let connection = establish_connection();
+    let len = todos.count().get_result::<i64>(&connection)?;
+    println!("{:#?}", len);
+    let deleted_row = diesel::delete(todos.filter(id.eq(len as i32))).execute(&connection);
+    Ok(())
 }
 
 //---------------------------------------------------------
